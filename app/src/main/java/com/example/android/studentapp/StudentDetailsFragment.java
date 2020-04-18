a1b3b20a79068e7010351233569eba2cf5c1150e
Ankita13-code <ankitapareek2000@gmail.com>
2020-04-18 11:40:25 +0530

    Project created

---
+++ app/src/main/java/com/example/android/studentapp/StudentDetailsFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.studentapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class StudentDetailsFragment extends Fragment {
+
+    public StudentDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_student_details, container, false);
+    }
+
+
+}
